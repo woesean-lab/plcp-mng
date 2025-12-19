3dfc105712ceb3d533bc43d1ba19d98f9711fda5
Uğur Yorulmaz <woesean@gmail.com>
2025-12-20 01:44:16 +0300

    Use message bubble icon for delivery copy button

--- src/App.jsx
+++ src/App.jsx
@@ -1074,8 +1074,9 @@ function App() {
                                 strokeWidth="2"
                                 className="h-4 w-4"
                               >
-                                <rect x="9" y="9" width="13" height="13" rx="2" ry="2" />
-                                <path d="M5 15H4a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h9a2 2 0 0 1 2 2v1" />
+                                <path d="M21 12c0 4-3.5 7-9 7-.8 0-1.6-.1-2.4-.3L3 20l1.4-3.1C3.5 16 3 14.8 3 13c0-4 3.5-7 9-7s9 3 9 7Z" />
+                                <path d="M9 13h6" />
+                                <path d="M8 10h8" />
                               </svg>
                             </button>
                             <button
