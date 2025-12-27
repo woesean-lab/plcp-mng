de0f65560c7f7a1d96a61833c17ee2bf09b55ac1
Uğur Yorulmaz <woesean@gmail.com>
2025-12-27 03:11:26 +0300

    Increase login preloader delay

--- src/hooks/useAppData.jsx
+++ src/hooks/useAppData.jsx
@@ -1269,7 +1269,7 @@ export default function useAppData() {
         console.warn("Could not persist auth token", error)
       }
       setAuthToken(token)
-      await delay(1700)
+      await delay(4700)
       setIsAuthed(true)
       setActiveUser(data?.user ?? null)
       setAuthUsername("")
